a225bf0241b0ee77c52ea32759b2939102de2f34
rserran <rserran@altec.com.ar>
2021-08-24 01:20:18 -0300

    RN config added

--- App.tsx
+++ App.tsx
@@ -4,9 +4,12 @@ import React from 'react';
 const {NavigationContainer} = require('@react-navigation/native');
 import MainNavigator from './src/navigation/MainNavigator';
 
+import Config from 'react-native-config';
+
 import {navigationRef} from './src/navigation/controls';
 
 const App = () => {
+  console.log(Config.API_URL);
   return (
     <NavigationContainer ref={navigationRef}>
       <MainNavigator />
